cff74eccdce41e3919ce484871bbd91689dc2cad
yvonne Josy Uyisenga <125833605+Chacade@users.noreply.github.com>
2023-04-25 16:41:34 +0200

    Update functions2.c
    print pointer, print non-printable and reverse then print string in rot13

--- functions2.c
+++ functions2.c
@@ -2,14 +2,14 @@
 
 /****************** PRINT POINTER ******************/
 /**
- * print_pointer - function
+ * print_pointer - function that print the value of a pointer variable
  * @types: arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed.
+ * Return: Number of chararacters printed.
  */
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
